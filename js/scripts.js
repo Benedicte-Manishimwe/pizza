1116bfa8ebecdb46b674a3cfc7797653ee6228fd
Florence <denuwise@gmail.com>
2019-08-07 18:44:33 +0200

    set the businees logic

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+//business logic
+function order(size,crust,topping,qte){
+    this.Size=size;
+    this.Crust=crust;
+    this.Topping=topping;
+    this.Quantities=qte;
+};
+order.prototype.placeOrder=function(){
+    return"you are ordering "+ this.Quantities+" pizza have the size:"+this.Size+ ",with crust: "+this.Crust+" , and the topping : "+this.Topping);
+};
+
+});
