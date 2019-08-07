feb8c8392831269631235c430611b84bf08148e3
Florence <denuwise@gmail.com>
2019-08-07 18:51:58 +0200

    add neworder option

--- js/scripts.js
+++ js/scripts.js
@@ -9,5 +9,6 @@ function order(size,crust,topping,qte){
 order.prototype.placeOrder=function(){
     return"you are ordering "+ this.Quantities+" pizza have the size:"+this.Size+ ",with crust: "+this.Crust+" , and the topping : "+this.Topping);
 };
-
+//User Interface logic
+$("")
 });
