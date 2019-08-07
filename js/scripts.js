06279abe169c8eb23feb1c89b4620f88f3435b3d
Florence <denuwise@gmail.com>
2019-08-07 20:42:54 +0200

    updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,51 @@
-$("document").ready(function(){
-//business logic
-function order(size,crust,topping,qte){
-    this.Size=size;
-    this.Crust=crust;
-    this.Topping=topping;
-    this.Quantities=qte;
-};
-order.prototype.placeOrder=function(){
-    return("you are ordering "+ this.Quantities+" pizza have the size:"+this.Size+ ",with crust: "+this.Crust+" , and the topping : "+this.Topping);
-};
-//User Interface logic
+$(document).ready(function () {
+    //business logic
+    function order(size, crust, toppings, qte) {
+        this.Size = size;
+        this.Crust = crust;
+        this.Topping = toppings;
+        this.Quantities = qte;
+    };
+    order.prototype.placeOrder = function () {
+        return ("you are ordering " + this.Quantities + " pizza have the size:" + this.Size + ",with crust: " + this.Crust + " , and the topping : " + this.Topping);
+    };
+    function person(names, address, phone) {
+        this.fullname = names;
+        this.location = address;
+        this.tel = phone;
+    }
+    client = new person(names, address, phone);
 
-$("#send").click(function(){
-$("#order").submit(function(event){
-    event.preventDefault();
-    var size=$("#Size").val();
-    var crust=$("#Crust").val();
-    console.log(size);
-    var toppings=[];
-    $("input[type=checkbox]:checked").each(function(){
-        toppings.push($(this).val());
+    //User Interface logic
+
+    $("#send").click(function () {
+        $("#order").submit(function (event) {
+            event.preventDefault();
+            var size = $("#Size").val();
+            var crust = $("#Crust").val();
+            var qte = $("#quantities").val();
+            var toppings = [];
+            $("input[type=checkbox]:checked").each(function () {
+                toppings.push($(this).val());
+
+                var newOrder = new order(size, crust, toppings, qte);
+                console.log(newOrder);
+                $("ul#view").append("<li>" + newOrder() + "</li>");
+                if (delivery === true) {
+                    alert("the delivery cost is 2000");
+                }
+
+                $("#comfirm").submit(function () {
+                    var total = parseInt(size) + parseInt(crust) + parseInt(toppings) + delivery;
+                    console.log(total);
+                    $("#price").append(total);
+                    var names = $("#fullname").val();
+                    var address = $("#location").val();
+                    var phone = $("#tel").val();
+                    var client = new person(names, address, phone)
+                    $("#motivation").append(client, "thank youm to shop with us!!")
+                });
+            
+        });
     });
-    console.log(toppings);
-    var newOrder=new order(size,crust,topping,qte);
-    console.log(newOrder);
-   
-    $("ul#view").append("<li>"+newOrder()+"</li>");
-    var total=parseInt(size)+parseInt(crust)+parseInt(toppings);
-    console.log(total);
-
-
-});
-});
 });
