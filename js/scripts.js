8b369cb4fa7fb35c8fb3cd3821c51f24d8d69690
Florence <denuwise@gmail.com>
2019-08-07 20:53:46 +0200

    add new updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,61 @@
-$(document).ready(function () {
-    //business logic
-    function order(size, crust, toppings, qte) {
-        this.Size = size;
-        this.Crust = crust;
-        this.Topping = toppings;
-        this.Quantities = qte;
-    };
-    order.prototype.placeOrder = function () {
-        return ("you are ordering " + this.Quantities + " pizza have the size:" + this.Size + ",with crust: " + this.Crust + " , and the topping : " + this.Topping);
-    };
-    function person(names, address, phone) {
-        this.fullname = names;
-        this.location = address;
-        this.tel = phone;
-    }
-    client = new person(names, address, phone);
+$("document").ready(function() {
+  //business logic
+  function order(size, crust, toppings, qte) {
+    this.Size = size;
+    this.Crust = crust;
+    this.Topping = toppings;
+    this.Quantities = qte;
+  }
+  order.prototype.placeOrder = function() {
+    return (
+      "you are ordering " +
+      this.Quantities +
+      " pizza have the size:" +
+      this.Size +
+      ",with crust: " +
+      this.Crust +
+      " , and the topping : " +
+      this.Topping
+    );
+  };
+  function person(names, address, phone) {
+    this.fullname = names;
+    this.location = address;
+    this.tel = phone;
+  }
+  client = new person(names, address, phone);
+  //User Interface logic
 
-    //User Interface logic
+  $("#send").click(function() {
+    $("#order").submit(function(event) {
+      event.preventDefault();
+      var size = $("#Size").val();
+      var crust = $("#Crust").val();
+      var qte = $("#quantities").val();
+      var toppings = [];
+      $("input[type=checkbox]:checked").each(function() {
+        toppings.push($(this).val());
 
-    $("#send").click(function () {
-        $("#order").submit(function (event) {
-            event.preventDefault();
-            var size = $("#Size").val();
-            var crust = $("#Crust").val();
-            var qte = $("#quantities").val();
-            var toppings = [];
-            $("input[type=checkbox]:checked").each(function () {
-                toppings.push($(this).val());
-
-                var newOrder = new order(size, crust, toppings, qte);
-                console.log(newOrder);
-                $("ul#view").append("<li>" + newOrder() + "</li>");
-                if (delivery === true) {
-                    alert("the delivery cost is 2000");
-                }
-
-                $("#comfirm").submit(function () {
-                    var total = parseInt(size) + parseInt(crust) + parseInt(toppings) + delivery;
-                    console.log(total);
-                    $("#price").append(total);
-                    var names = $("#fullname").val();
-                    var address = $("#location").val();
-                    var phone = $("#tel").val();
-                    var client = new person(names, address, phone)
-                    $("#motivation").append(client, "thank youm to shop with us!!")
-                });
-            
-        });
+        var newOrder = new order(size, crust, toppings, qte);
+        console.log(newOrder);
+        $("ul#view").append("<li>" + newOrder() + "</li>");
+        if (delivery === true) {
+          alert("the delivery cost is 2000");
+        }
     });
+        $("#comfirm").submit(function() {
+          var total =
+            parseInt(size) + parseInt(crust) + parseInt(toppings) + delivery;
+          console.log(total);
+          $("#price").append(total);
+          var names = $("#fullname").val();
+          var address = $("#location").val();
+          var phone = $("#tel").val();
+          var client = new person(names, address, phone);
+          $("#motivation").append(client, "thank youm to shop with us!!");
+        });
+      });
+    
+  });
 });
+
